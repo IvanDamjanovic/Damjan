fb54680c14ed5e7140728244663bac2676aa6210
IvanDamjanovic <57716218+IvanDamjanovic@users.noreply.github.com>
2019-11-21 23:53:13 +0100

    Update skriptapp20.sql

--- skriptapp20.sql
+++ skriptapp20.sql
@@ -5,48 +5,48 @@ create database edunovapp20 default character set utf8;
 # c:\xampp\mysql\bin\mysql.exe -uedunova -pedunova --default_character_set=utf8 < f:\skriptapp20.sql
 use edunovapp20;
 create table smjer(
-sifra       int not null primary key auto_increment,
-naziv       varchar(50) not null,
-trajanje    int,
-cijena      decimal(18,2) not null,
-upisnina    decimal(18,2) not null,
-verificiran boolean default true
+    sifra           int not null primary key auto_increment,
+    naziv           varchar(50) not null,
+    trajanje        int,
+    cijena          decimal(18,2) not null,
+    upisnina        decimal(18,2) not null,
+    verificiran     boolean default true
 );
 
 
 create table grupa(
-sifra int not null primary key auto_increment,
-naziv varchar(20) not null,
-smjer int not null,
-predavac int ,
-brojpolaznika int not null,
-datumpocetka datetime 
+    sifra           int not null primary key auto_increment,
+    naziv           varchar(20) not null,
+    smjer           int not null,
+    predavac        int,
+    brojpolaznika   int not null,
+    datumpocetka    datetime 
 );
 
 create table predavac(
-    sifra int not null primary key auto_increment,
-    osoba int not null,
-    iban varchar(50)
+    sifra           int not null primary key auto_increment,
+    osoba           int not null,
+    iban            varchar(50)
 );
 
 create table osoba(
-    sifra int not null primary key auto_increment,
-    ime varchar(50) not null,
-    prezime varchar(50) not null,
-    oib char(11),
-    email varchar(100) not null
+    sifra           int not null primary key auto_increment,
+    ime             varchar(50) not null,
+    prezime         varchar(50) not null,
+    oib             char(11),
+    email           varchar(100) not null
 );
 
 
 create table polaznik(
-    sifra int not null primary key auto_increment,
-    osoba int not null,
-    brojugovora varchar(50)
+    sifra           int not null primary key auto_increment,
+    osoba           int not null,
+    brojugovora     varchar(50)
 );
 
 create table clan(
-    grupa int not null,
-    polaznik int not null
+    grupa           int not null,
+    polaznik        int not null
 );
 
 
