2bbd96a38e6c77324040c32c7ce4e13e6fc5c497
IvanDamjanovic <57716218+IvanDamjanovic@users.noreply.github.com>
2020-03-31 10:45:28 +0200

    RedBook update

--- controller/IstrazivacController.php
+++ controller/IstrazivacController.php
@@ -2,17 +2,37 @@
 
 class IstrazivacController extends AdminController
 {
+
+    private $viewDir = 'privatno' . 
+    DIRECTORY_SEPARATOR . 'istrazivac' . 
+    DIRECTORY_SEPARATOR;
+    
     public function index()
     {
-        $veza = DB::getInstanca();
-        $izraz = $veza->prepare('select * from istrazivac');
-        $izraz->execute();
-        $rezultati = $izraz->fetchAll();
-
-        $this->view->render('privatno' . 
-     DIRECTORY_SEPARATOR . 'istrazivac' .
-     DIRECTORY_SEPARATOR . 'index',[
-        'podaci'=>$rezultati
+        $this->view->render($this->viewDir . 'index',[
+            'podaci'=>istrazivac::read()
      ]);
+    }
+
+    public function novi()
+    {
+        $this->view->render($this->viewDir . 'novi',
+        ['poruka' => 'Popunite sve tražene podatke']
+    );
+    }
+
+    public function dodajnovi()
+    {
+        //prvo dođu sve silne kontrole
+        Istrazivac::create();
+        $this->index();
+    }
+
+    public function obrisi()
+    {
+        //prvo dođu silne kontrole
+        Istrazivac::delete();
+        //$this->index();
+        header('location: /istrazivac/index');
     }
 }
