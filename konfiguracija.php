4a8169c31e0b13e5ef8c1c79a72378b0418416c9
IvanDamjanovic <57716218+IvanDamjanovic@users.noreply.github.com>
2020-04-04 16:30:41 +0200

    Update konfiguracija.php

--- konfiguracija.php
+++ konfiguracija.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 return [
     'nazivAPP' => 'RedBook',
     'url' => 'http://redbook.hr/',
@@ -10,4 +10,17 @@ return [
         'korisnik' => 'edunova',
         'lozinka' => 'edunova'
     ]
+*/
+
+return [
+    'nazivAPP' => 'RedBook',
+    'url' => 'http://polaznik37.edunova.hr/',
+    'dev' => true,
+    'db' =>[
+        'server' => 'localhost',
+        'baza' => 'redbook',
+        'korisnik' => 'eskulap_edunova',
+        'lozinka' => 'edunova'
+
+
 ];
